0f2fa569f02560f3ccbfc5df443bc5ccdb38843c
Frode Grimstad Bang <f@frodebang.com>
2012-09-29 14:33:56 +0200

    removed stuff from previous project

--- data_model.py
+++ data_model.py
@@ -3,13 +3,6 @@
 
 from google.appengine.ext import db
 
-class crew_member(db.Model):
-	pseudonym = db.StringProperty()
-	username = db.StringProperty()
-	profile_views = db.IntegerProperty(default=0)
-	last_modified = db.DateTimeProperty(auto_now=True)
-	created = db.DateTimeProperty(auto_now_add=True)
-
 #class attribute(db.Model):
 #	title = db.StringProperty()
 #	category_key = db.ReferenceProperty(attribute_category)
